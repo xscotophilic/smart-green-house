4d12a3e3443a71ab609488edaf3ba83382b2bd2d
chintan <chintan@chintans-MacBook-Air.local>
2020-12-26 10:53:27 +0530

    fe route fix 0

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const keys = require('./config/keys');
+const mongoose = require("mongoose");
+const express = require("express");
+const keys = require("./config/keys");
 
 // creating app
 const app = express();
 
 // connect to the database
 mongoose.connect(keys.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // processing req
 app.use(express.json());
 
 // route handlers
-app.use('/api', require('./routes/api'));
+app.use("/api", require("./routes/api"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // port and listening
 const PORT = process.env.PORT || 5000;
